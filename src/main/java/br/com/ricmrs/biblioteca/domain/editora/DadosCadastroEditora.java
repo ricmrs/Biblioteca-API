5a95571493b33762b2c0cee267e26fea9f4320df
Ricardo <83619661+ricmrs@users.noreply.github.com>
2023-12-04 07:48:24 -0300

    Add @NotBlank annotation

--- src/main/java/br/com/ricmrs/biblioteca/domain/editora/DadosCadastroEditora.java
+++ src/main/java/br/com/ricmrs/biblioteca/domain/editora/DadosCadastroEditora.java
@@ -1,6 +1,9 @@
 package br.com.ricmrs.biblioteca.domain.editora;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record DadosCadastroEditora(
+        @NotBlank
         String nome
 ) {
 }
